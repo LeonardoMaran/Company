fa6f307e7db25ce275492e4fab867e697f71ec80
man_tutu <471889225@qq.com>
2017-01-24 15:38:35 +0800

    单选下拉框

--- app/components/SelectBox.jsx
+++ app/components/SelectBox.jsx
@@ -3,82 +3,58 @@ import './SelectBox.sass';
 
 export default class SelectBox extends Component {
   static propTypes = {
-    placeholder: PropTypes.string,
     options: PropTypes.array,
-    value: PropTypes.array,
+    value: PropTypes.string,
     defaultValue: PropTypes.string,
-    onChange: PropTypes.func
+    onChange: PropTypes.func,
+    firstSelected: PropTypes.bool
   };
 
-  state = {values: this.props.value || [], show: false};
-
-  clickingOption = false;
+  state = {value: this.props.value || this.props.defaultValue, option: {}, show: false};
 
   get value() {
-    return this.state.values;
+    return this.state.value;
+  }
+
+  close = (option) => {
+    if(option) {
+      this.setState({option});
+    }
+    this.setState({show: false});
   }
 
   toggle = () => {
     this.setState({show: !this.state.show});
-  };
-
-  close = () => {
-    if (this.clickingOption) return;
-    this.setState({show: false});
-  };
-
-  checkChange = (value) => {
-    return (event) => {
-      this.refs.inputBox.focus();
-      this.clickingOption = false;
-      if (event.target.checked) {
-        this.state.values.push(value);
-      } else {
-        this.state.values.remove(value);
-      }
-      this.forceUpdate();
-      this.refs.inputBox.value = this.state.values.join('，');
-      this.props.onChange && this.props.onChange(this.value);
-    };
-  };
-
-  optionMouseDown = () => {
-    this.clickingOption = true;
-  }
-
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.value != this.value) {
-      this.setState({values: nextProps.value || []});
-    }
   }
 
   render() {
     const {
+      width,
       options,
       value,
-      placeholder,
       defaultValue,
       onChange,
+      firstSelected,
+      children,
       ...other
     } = this.props;
 
-    let values = this.state.values;
-
     let createOption = (option, i) => {
-      let {label, value} = typeof(option) == 'string' ? {label: option, value: option} : option;
-      let checked = values.indexOf(value) > -1;
-      return <li key={i}><label onMouseDown={this.optionMouseDown}><input type="checkbox" checked={checked} onChange={this.checkChange(value)}/>{label}</label></li>;
-    };
-
-    const overlayProps = {
-      show: this.state.show,
-      container: this,
-      target: () => ReactDOM.findDOMNode(this.refs.inputBox)
+      let {label, value, disabled} = typeof(option) == 'string' ? {label: option, value: option} : option;
+      return <li key={i} onClick={() => this.close(option)}>{label}</li>
     };
 
     return (
-      <div className="select-box" style={{width: this.props.width}}>
-
+      <div className="select-box" style={{width: width}}>
+        <li className="title" onClick={this.toggle}><label>{this.state.option.label}</label></li>
+        {
+          this.state.show
+          ? <ul>
+              {options.map(createOption)}
+              {children}
+            </ul>
+          : null
+        }
       </div>
     );
   }

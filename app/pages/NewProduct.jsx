f03744ea089e6b75ec476b6e8d50c18307413af1
man_tutu <471889225@qq.com>
2017-02-10 14:53:02 +0800

    新建产品 产品详情 产品列表

--- app/pages/NewProduct.jsx
+++ app/pages/NewProduct.jsx
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 import {Link, browserHistory} from 'react-router';
 import Tabs from '../components/Tabs';
+import TextInput from '../components/TextInput';
 import Session from '../lib/Session';
 import ApiCaller from '../lib/ApiCaller';
 
 export default class NewProduct extends Component {
-  state = {message: ''};
+  state = {message: '', product: {}};
 
   save  = () => {
     ApiCaller.postData(`/api/product`, {
@@ -19,16 +20,28 @@ export default class NewProduct extends Component {
     })
   }
 
+  componentDidMount() {
+    let pathname = this.props.route.path;
+    if(pathname.split('/')[1] == "edit") {
+      ApiCaller.loadData(`/api/product/${this.props.params.id}`, {}, (result, error) => {
+        if(result.status == 200) {
+          this.setState({product: result.body});
+        }
+      });
+    }
+  }
+
   render() {
     //单击去定义新产品.
     //您必须为销售的所有内容定义产品，无论是实体产品，消耗品还是为客户提供的服务。
     //产品表单中的信息可以简化销售流程: 价格，询价单中的注释，会计信息，补货方式等等
+    let product = this.state.product;
     return (
       <div id="new_product">
         <h1>产品 / 新建</h1>
         <button onClick={this.save} className="btn btn-primary" style={{marginBottom: '10px'}}>保存</button>
         <p>产品名称</p>
-        <h1><input ref="name" placeholder="产品名称" /></h1>
+        <h1><TextInput ref="name" value={product.name} placeholder="产品名称" /></h1>
         <p><input type="checkbox" />销售</p>
         <p><input type="checkbox" />采购</p>
         <img />
@@ -38,6 +51,12 @@ export default class NewProduct extends Component {
           </div>
           <div title="销售">
             22
+          </div>
+          <div title="备注">
+            <h1>报价单说明</h1>
+            <input />
+            <h1>供应商说明</h1>
+            <input />
           </div>
         </Tabs>
         <p className="error">{this.state.message}</p>
--- app/pages/Product.jsx
+++ app/pages/Product.jsx
@@ -1,47 +1,66 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router';
 import DataTable from '../components/DataTable';
+import Dialog, {DialogHeader, DialogBody, DialogFooter} from '../components/Dialog';
 import ApiCaller from '../lib/ApiCaller';
 import '../styles/Product.sass';
 
 export default class Product extends Component {
-  state = {products: [], message: ''};
+  state = {products: [], message: '', productId: null, status: "loading"};
 
-  componentDidMount() {
+  getProduct = () => {
     ApiCaller.loadData(`/api/product`, {}, (result, error) => {
       if(result.status == 200) {
-        this.setState({products: result.body.item});
+        let products = result.body.item;
+        this.setState({products, status: products.isEmpty() ? "empty" : "nonempty"});
       } else {
         this.setState({message: result.text});
       }
+    });
+  }
+
+  openDelete = (productId) => {
+    this.setState({productId});
+    this.refs.delete.open();
+  };
+
+  deleteProduct = () => {
+    ApiCaller.deleteData(`/api/product/${this.state.productId}`, {}, (result, error) => {
+      if(result.status == 200) {
+        this.getProduct();
+        this.refs.delete.close();
+      } else {
+        this.setState({message: '要删除产品，你必须先取消它'});
+      }
     })
+  };
+
+  componentDidMount() {
+    this.getProduct();
   }
 
   render() {
     let fields = [];
-    console.log(this.props.location.pathname.split('/')[1]);
-    switch(this.props.location.pathname.split('/')[1]) {
+    let pathname = this.props.location.pathname.split('/')[1];
+    switch(pathname) {
       case 'product':
       case 'purchase':
+      case 'sales':
         fields = [
-         {label: '内部参考', name: 'status'},
+         {label: '内部参考', name: 'id'},
          {label: '名字', name: 'name'},
          {label: '标价', name: 'status'},
          {label: '成本', name: 'status'},
          {label: '内部类别', name: 'status'},
          {label: '产品类型', name: 'status'},
          {label: '在手数量', name: 'status'},
-         {label: '预测数量', name: 'status'}
-        ];
-        break;
-      case 'sales':
-        fields = [
-         {label: '内部参考', name: 'status'},
-         {label: '名字', name: 'name'},
-         {label: '标价', name: 'status'},
-         {label: '成本', name: 'status'},
-         {label: '内部类别', name: 'status'},
-         {label: '产品类型', name: 'status'}
+         {label: '预测数量', name: 'status'},
+         {label: '操作', compute: item =>
+           <div>
+             <Link style={{margin: '5px'}} to={`/${pathname}/product/${item.id}`} className="btn btn-default">查看详情</Link>
+             <button onClick={() => this.openDelete(item.id)} className="btn btn-primary">删除</button>
+           </div>
+         }
         ];
         break;
     }
@@ -49,8 +68,17 @@ export default class Product extends Component {
     return (
       <div id="product">
         <h1>产品</h1>
-        <Link to="/sales/product/new" className="btn btn-primary" style={{marginBottom: '10px'}}>创建</Link>
+        <Link to={`/${pathname}/product/new`} className="btn btn-primary" style={{marginBottom: '10px'}}>创建</Link>
 				<DataTable fields={fields} items={this.state.products} />
+
+        <Dialog ref="delete">
+          <DialogHeader> 删除产品 </DialogHeader>
+          <DialogBody> 确认删除 </DialogBody>
+          <DialogFooter>
+            <button className="btn btn-primary" onClick={this.deleteProduct}>删除</button>
+            <button className="btn btn-default" onClick={() => this.refs.delete.close()}>取消</button>
+          </DialogFooter>
+        </Dialog>
       </div>
     );
   }
